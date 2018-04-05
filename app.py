1b5490068f34a7af63332a8653fc795ebc17aff6
Mnnu-Ann <minnuann5@gmail.com>
2018-04-06 00:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") = "trainStatus":
-        return "success"
+    if req.get("result").get("action") == "trainStatus":
+        return "nothing"
     baseurl = "https://api.railwayapi.com/v2/live/train/17229/date/05-04-2018/apikey/e5hkcdzqsj/"
 
     yql_query = makeYqlQuery(req)
