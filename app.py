cf9d033446ac82b833d04b78c53fd39175e8a7b0
Mnnu-Ann <minnuann5@gmail.com>
2018-04-09 23:27:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def processRequest(req):
         return {}
     baseurl = "https://api.railwayapi.com/v2/live/train/" 
     i = datetime.datetime.now()
-    today = %(i.day, i.month, i.year)
+    today = i.day +"-"+ i.month +"-"+ i.year
     remain = "/date/"+today+"/apikey/e5hkcdzqsj/"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
