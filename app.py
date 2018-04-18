adb7a94fe86e17b94ebac00c4ea412e19074e2dd
Mnnu-Ann <minnuann5@gmail.com>
2018-04-18 13:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from flask import make_response
 app = Flask(__name__)
 
 #----------------------------------------Main Entry Point---------------------------------------------------
-apikey = "e5hkcdzqsj"
+apikey = "1f8y1ujgm5"
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
