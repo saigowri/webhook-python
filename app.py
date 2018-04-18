c518b72fb94d349d6860d38eb5b1b6bcdbcd62c5
saigowri <saigowri.kumar@gmail.com>
2018-04-19 02:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,8 +235,8 @@ def processCancelledTrains(req):
 
 def makeWebhookResultStatus(data):
     speech = data.get('position')
-    if not data.get('position'):
-        speech = "Train may be cancelled"
+    if not data.get('position') and data.get('response_code')==210:
+        speech = "Train may be cancelled or is not scheduled to run"
     return {
         "speech": speech,
         "displayText": speech,
