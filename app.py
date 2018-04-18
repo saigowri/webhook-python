3870a8cbb085017fabbc05dc3e819a59c4c22827
saigowri <saigowri.kumar@gmail.com>
2018-04-18 18:22:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,11 +300,16 @@ def makeWebhookResultForFARE(data):
 def makeWebhookResultForCancelled(data, trainName, date):
     msg = []
     speech = ""
+    flag = 0
     for train in data['trains']:
 	if trainName == train['name']:
 		speech = train['name'] + " having train number " + train['number'] + " is cancelled on " + date
         	msg.append( train['name'] + " having train number " + train['number'] + " is cancelled on " + date)
+		flag = 1
 		break
+    if flag == 0:
+	speech = trainName + " is not cancelled on " + date
+        msg.append( trainName + " is not cancelled on " + date)
     messages = [{"type": 0, "speech": s[0]} for s in zip(msg)]
     reply = {
             "speech": speech,
