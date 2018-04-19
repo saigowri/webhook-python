c9f3daa6ec97ed2d8157cb5f5931cab661d5393f
KavyaKPuranik <kpuranik08@gmail.com>
2018-04-19 22:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,16 +240,16 @@ def processPNRStatus(req):
         return {}
     baseurl = "https://api.railwayapi.com/v2/pnr-status/pnr/" 
     remain = "/apikey/"+apikey
-	result = req.get("result")
-    parameters = result.get("parameters")
+    rest = req.get("result")
+    parameters = rest.get("parameters")
     pnrnum = parameters.get("number-integer")
     if pnrnum is None:
         return {}
     yql_url = baseurl + pnrnum + remain
     result = urlopen(yql_url).read()
     data = json.loads(result)    
-	chart_prepared = data.get('chart_prepared')
-	speech = "The chart has been prepared: "+ chart_prepared
+    chart_prepared = data.get('chart_prepared')
+    speech = "The chart has been prepared: "+ chart_prepared
 #    if data.get('response_code') == 210:
 #        speech = "Train may be cancelled or is not scheduled to run"
     return {
