7dd8f1ad33e41cafe5c3669f46c72fffa2626644
Mnnu-Ann <minnuann5@gmail.com>
2018-04-23 00:08:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ def processRescheduledTrains(req):
     c =  json.dumps(date) 
     print("date: "+c)
     trainvar = ""
-    trainname = "PUNE-GKP" #parameters.get("Train_name")
+    trainname = parameters.get("Train_name")
     if trainname:
         yql_query_train = trainname
         trainvar = 'name'
